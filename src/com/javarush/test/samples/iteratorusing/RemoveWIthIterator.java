8397c082f036491cfc0f7d3a3c489b16f177e86c
prulov <prulov.pr@gmail.com>
2016-09-07 18:26:05 +0300

    commit 06092016

---
+++ src/com/javarush/test/samples/iteratorusing/RemoveWIthIterator.java
@@ -0,0 +1,59 @@
+package com.javarush.test.samples.iteratorusing;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by prulov on 07.09.2016.
+ */
+public class RemoveWIthIterator
+{
+    public static void main(String[] args)
+    {
+        List<String> list = new ArrayList<>();
+        list.add("Soroka");
+        list.add("-");
+        list.add("");
+        list.add("vorovka ");
+        list.add("");
+        list.add("kashu ");
+        list.add("");
+        list.add("varila");
+        list.add(", ");
+        list.add("detok ");
+        list.add("");
+        list.add("kormila.");
+
+        for (Iterator<String> iterator = list.iterator(); iterator.hasNext();) {
+            String string = iterator.next();
+            if (string.isEmpty()) {
+                // Remove the current element from the iterator and the from the list.
+                iterator.remove();
+            }
+        }
+
+        for(String str : list) System.out.print(str);
+        System.out.println();
+
+
+        //INTERESTING METHOD collection1.removeAll(collection2)
+
+        Collection<Integer> l = new ArrayList<Integer>();
+        Collection<Integer> itemsToRemove = new ArrayList<Integer>();
+        for (int i=0; i < 10; ++i) {
+            l.add(new Integer(4));
+            l.add(new Integer(5));
+            l.add(new Integer(6));
+        }
+        for (Integer i : l)
+        {
+            if (i.intValue() == 5)
+                itemsToRemove.add(i);
+        }
+
+        l.removeAll(itemsToRemove);
+        System.out.println(l);
+    }
+}

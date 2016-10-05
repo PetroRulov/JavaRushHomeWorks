2b8768a50601e713f87ad4c7feadfe379892359f
prulov <prulov.pr@gmail.com>
2016-10-05 18:08:57 +0300

    commit 05102016

--- src/com/javarush/test/level09/lesson11/bonus03/Solution.java
+++ src/com/javarush/test/level09/lesson11/bonus03/Solution.java
@@ -3,6 +3,9 @@ package com.javarush.test.level09.lesson11.bonus03;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /* Задача по алгоритмам
 Задача: Пользователь вводит с клавиатуры список слов (и чисел). Слова вывести в возрастающем порядке, числа - в убывающем.
@@ -50,7 +53,114 @@ public class Solution
 
     public static void sort(String[] array)
     {
-        //напишите тут ваш код
+//        Map<Integer, String> digits = new HashMap<>();
+//        Map<Integer, String> words = new HashMap<>();
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if(isNumber(array[i])){
+//                digits.put(i, array[i]);
+//            }else{
+//                words.put(i, array[i]);
+//            }
+//        }
+//
+//        List<String> sortedDigits = new ArrayList<>();
+//        for(Map.Entry<Integer, String> pair : digits.entrySet()) sortedDigits.add(pair.getValue());
+//        String[] arrayDigits = sortedDigits.toArray(new String[sortedDigits.size()]);
+//        boolean flag = true;
+//        String temp;
+//        while(flag){
+//            flag=false;
+//            for (int j = 0; j < arrayDigits.length - 1; j++) {
+//                if( Integer.parseInt(arrayDigits[j]) < Integer.parseInt(arrayDigits[j+1])){
+//                    temp = arrayDigits[j];
+//                    arrayDigits[j] = arrayDigits[j+1];
+//                    arrayDigits[j+1]=temp;
+//                    flag=true;
+//                }
+//            }
+//        }
+//
+//        List<String> sortedWords = new ArrayList<>();
+//        for(Map.Entry<Integer, String> pair : words.entrySet()) sortedWords.add(pair.getValue());
+//        String[] arrayWords = sortedWords.toArray(new String[sortedDigits.size()]);
+//        flag = true;
+//        while(flag){
+//            flag=false;
+//            for (int j = 0; j < words.size() - 1; j++) {
+//                if( isGreaterThan(arrayWords[j].toLowerCase(), arrayWords[j+1].toLowerCase())){
+//                    temp=arrayWords[j];
+//                    arrayWords[j] = arrayWords[j+1];
+//                    arrayWords[j+1] = temp;
+//                    flag=true;
+//                }
+//            }
+//        }
+//
+//        digits.clear();
+//        words.clear();
+//
+//        int j = 0, k = 0;
+//        for (int i = 0; i < array.length; i++)
+//        {
+//            if(isNumber(array[i])){
+//                digits.put(i, arrayDigits[j]);
+//                if(j < arrayDigits.length) j++;
+//            }else{
+//                words.put(i, arrayWords[k]);
+//                if(k < arrayWords.length) k++;
+//            }
+//        }
+//
+//        for(Map.Entry<Integer, String> pair : words.entrySet()){
+//            digits.put(pair.getKey(), pair.getValue());
+//        }
+//
+//        int i = 0;
+//        for(Map.Entry<Integer, String> pair : digits.entrySet()){
+//            array[i] = pair.getValue();
+//            i++;
+//        }
+
+        ArrayList<Integer> stringList = new ArrayList<>();
+        ArrayList<Integer> intList = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++)
+        {
+            if (isNumber(array[i])) {
+                intList.add(i);
+            } else {
+                stringList.add(i);
+            }
+        }
+
+        for (int i = (stringList.size() - 1); i > 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                int num = stringList.get(j);
+                int numNext = stringList.get(j + 1);
+                if (isGreaterThan(array[num], array[numNext])) {
+                    String tmp = array[num];
+                    array[num] = array[numNext];
+                    array[numNext] = tmp;
+                }
+            }
+        }
+
+        for (int i = (intList.size() - 1); i > 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                int num = intList.get(j);
+                int numNext = intList.get(j + 1);
+                if (Integer.parseInt(array[numNext]) > Integer.parseInt(array[num])) {
+                    String tmp = array[num];
+                    array[num] = array[numNext];
+                    array[numNext] = tmp;
+                }
+            }
+        }
     }
 
     //Метод для сравнения строк: 'а' больше чем 'b'

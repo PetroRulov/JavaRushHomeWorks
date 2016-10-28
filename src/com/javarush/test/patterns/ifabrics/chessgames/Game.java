f8436a0d0235e30c99a0ebf658c0c85184a9d33e
prulov <prulov.pr@gmail.com>
2016-10-28 15:27:31 +0300

    commit 28102016-1

---
+++ src/com/javarush/test/patterns/ifabrics/chessgames/Game.java
@@ -0,0 +1,30 @@
+package com.javarush.test.patterns.ifabrics.chessgames;
+
+interface Game { boolean move(); }
+interface GameFactory { Game getGame(); }
+
+class Checkers implements Game {
+    private int moves = 0;
+    private static final int MOVES = 3;
+    public boolean move() {
+        System.out.println("Checkers move " + moves);
+        return ++moves != MOVES;
+    }
+}
+
+class CheckersFactory implements GameFactory {
+    public Game getGame() { return new Checkers(); }
+}
+
+class Chess implements Game {
+    private int moves = 0;
+    private static final int MOVES = 4;
+    public boolean move() {
+        System.out.println("Chess move " + moves);
+        return ++moves != MOVES;
+    }
+}
+
+class ChessFactory implements GameFactory {
+    public Game getGame() { return new Chess(); }
+}
